d9d40947bd1d2f97ece207c9949e542f2bbaf229
Roger <138404274+Kouriin1@users.noreply.github.com>
2025-11-08 16:59:27 -0400

    Feat(fix) file structure

--- src/routes/Bp_modify.py
+++ src/routes/Bp_modify.py
@@ -4,23 +4,6 @@ from keys import supabase
 Bp_modify = Blueprint('Bp_modify', __name__)
 
 
-# @Bp_modify.route('/<int:user_id>', methods=['GET'])
-# def get_single_user(user_id):
-#     try:
-#         response = supabase.table('User').select('*').eq('id_user', user_id).execute()
-        
-#         user_data = response.data
-        
-#         if user_data:
-#             return jsonify(user_data[0]), 200
-#         else:
-#             return jsonify({"message": f"User with ID {user_id} not found"}), 404
-
-#     except Exception as e:
-#         print(f"ERROR DE SUPABASE: {e}")
-#         return jsonify({"message": "Internal Server Error"}), 500
-
-
 @Bp_modify.route('/user/update/<int:user_id>', methods=['PUT'])
 def update_user(user_id):
     try:
@@ -50,17 +33,3 @@ def update_user(user_id):
         print(f"ERROR DE SUPABASE: {e}")
         return jsonify({"message": "Internal Server Error"}), 500
 
-
-# @Bp_modify.route('/<int:user_id>', methods=['DELETE'])
-# def delete_user(user_id):
-#     try:
-#         response = supabase.table('User').delete().eq('id_user', user_id).execute()
-        
-#         if response.data:
-#             return jsonify({"message": "User deleted successfully"}), 200
-#         else:
-#             return jsonify({"message": "User not found"}), 404
-
-#     except Exception as e:
-#         print(f"ERROR DE SUPABASE: {e}")
-#         return jsonify({"message": "Internal Server Error"}), 500
