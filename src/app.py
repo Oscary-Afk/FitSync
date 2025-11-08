8f537f1ceea8d43027e142d4c4c7925e7efcf1cb
josefrometa <josel.frometa@gmail.com>
2025-11-08 14:59:03 -0400

    feat(fixed) compatibily problems solved and adding sign up

--- src/app.py
+++ src/app.py
@@ -1,6 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from routes.Bp_modify import Bp_modify
+from routes.sign_up import sign_up
+from routes.delete_user import delete_user
+from routes.get_users import get_users
 from keys import supabase
 from dotenv import load_dotenv
 import os
@@ -11,6 +14,9 @@ app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(Bp_modify, url_prefix='/api/user')
+app.register_blueprint(sign_up, url_prefix='/signup')
+app.register_blueprint(delete_user, url_prefix='/user/delete')
+app.register_blueprint(get_users, url_prefix= '/user')
 
 @app.route('/api/users', methods=['GET'])
 def get_all_users():
@@ -27,5 +33,38 @@ def get_all_users():
         print(f"ERROR DE SUPABASE: {e}")
         return jsonify({"message": "Internal Server Error"}), 500
 
+
+# @app.route('/signup', methods=['POST'])
+# def signnup():
+#     try:
+#         data = request.get_json()
+
+#         name = data.get("name")
+#         last_name = data.get("last_name")
+#         email = data.get("email")
+#         password_encrypted = data.get("password_encrypted")
+#         number = data.get("number")
+#         # id_rol = 1
+
+#         if not name or not last_name or not email or not password_encrypted or not number:
+#             return jsonify("message: Faltan campos por llenar"), 400
+        
+#         response = supabase.table("User").insert({
+#                 "name": name,
+#                 "last_name": last_name,
+#                 "email": email,
+#                 "password_encrypted": password_encrypted,
+#                 "number":number
+#                 # "id_rol": id_rol
+#         }).execute()
+
+#         if response.data:
+#             return jsonify({"message": "Usuario creado exitosamente", "user": response.data[0]}), 201
+#         else:
+#             return jsonify({"message": "error al crear usuario"}), 500
+        
+#     except Exception as e:
+#         print(f"ERROR al registrar usuario: {e}")
+#         return jsonify({"message": str(e)}), 500
 if __name__ == '__main__':
     app.run(debug=True)
