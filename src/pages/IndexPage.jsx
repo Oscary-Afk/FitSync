89916bc07beb4c378c9577ea84de8c18f7bbb356
elias <wayelias13@gmail.com>
2025-11-20 20:09:51 -0400

    colocando front dashboard

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,11 +1,14 @@
 import NavbarLoged from '../components/NavbarLoged.jsx';
 import Footer from '../components/Footer.jsx';
+import Dashboard from '../components/Dashboard.jsx';
+import Payment from '../components/Payment.jsx';
 
 export function IndexPage() {
   return (
     <>
       <NavbarLoged />
       <Dashboard />
+      <Payment />
       <Footer />
     </>
   );
